cca31170216d27acec455f9af4cc827eca2100bb
user <user@192.168.1.117>
2017-11-01 16:37:06 +0800

    WDYB WEB工程

---
+++ WDYB-Platform-Web/src/main/java/com/wdyb/WdybPlatformWebApplication.java
@@ -0,0 +1,12 @@
+package com.wdyb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WdybPlatformWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WdybPlatformWebApplication.class, args);
+	}
+}
