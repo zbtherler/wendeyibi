7d5ed891dd16d219634eac03af6e85c51b671842
user <user@192.168.1.117>
2017-11-01 18:27:47 +0800

    WDYB api接口工程

---
+++ WDYB-Api/src/main/java/com/wdyb/WdybApiApplication.java
@@ -0,0 +1,12 @@
+package com.wdyb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WdybApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WdybApiApplication.class, args);
+	}
+}
