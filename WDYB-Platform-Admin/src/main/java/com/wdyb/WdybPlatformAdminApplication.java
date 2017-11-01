fe8ee8748a9e78b0f250550bc9af04bd2a593ddb
user <user@192.168.1.117>
2017-11-01 16:36:07 +0800

    WDYB后台管理

---
+++ WDYB-Platform-Admin/src/main/java/com/wdyb/WdybPlatformAdminApplication.java
@@ -0,0 +1,12 @@
+package com.wdyb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WdybPlatformAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WdybPlatformAdminApplication.class, args);
+	}
+}
