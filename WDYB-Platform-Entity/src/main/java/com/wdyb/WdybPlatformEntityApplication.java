7cee3e509c0e135f907098dff1a4ecab644e70ab
user <user@192.168.1.110>
2017-11-03 14:27:43 +0800

    WDYB实体工程

---
+++ WDYB-Platform-Entity/src/main/java/com/wdyb/WdybPlatformEntityApplication.java
@@ -0,0 +1,12 @@
+package com.wdyb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WdybPlatformEntityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WdybPlatformEntityApplication.class, args);
+	}
+}
